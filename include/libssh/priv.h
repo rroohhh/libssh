d37d47ffbc84bc8981f1d64b3f8cfaa044da4b93
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2017-02-15 15:31:08 +0100

    fix clang compilation

--- include/libssh/priv.h
+++ include/libssh/priv.h
@@ -332,6 +332,8 @@ int ssh_connector_remove_event(ssh_connector connector);
  *    warning C4003: not enough actual parameters for macro '_VA_ARG_N'
  *  and incorrect behavior. This fixes issue.
  */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #define VA_APPLY_VARIADIC_MACRO(macro, tuple) macro tuple
 
 #define __VA_NARG__(...) \
@@ -354,6 +356,7 @@ int ssh_connector_remove_event(ssh_connector connector);
         29, 28, 27, 26, 25, 24, 23, 22, 21, 20, \
         19, 18, 17, 16, 15, 14, 13, 12, 11, 10, \
          9,  8,  7,  6,  5,  4,  3,  2,  1,  0
+#pragma clang diagnostic pop
 #else
 /* clang does not support the above construction */
 #define __VA_NARG__(...) (-1)
